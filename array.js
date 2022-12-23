3f46f31bcc8ce1297d4a52adcd94e183e2caa172
Saydina <saydinambiya@gmail.com>
2022-12-23 16:47:49 +0700

    [ADD] array javasript

---
+++ array.js
@@ -0,0 +1,7 @@
+let arr = [1, undefined, null, "Josh"];
+arr[4] = "Esyeden";
+arr.push("Enigma");
+arr.pop();
+arr.splice(0, 3);
+arr.slice(1, 2);
+console.log(arr);
---
+++ arraySort.js
@@ -0,0 +1,22 @@
+// function urutan() {
+//     let arr = ["warung", "ke", "jalan", "saya"];
+//     let result = [];
+//     for (let i = arr.length - 1; i >= 0; i--) { //i=2; 2>=0;1
+//         result.push(arr[i]);
+//     }
+//     console.info(result);
+// }
+
+// urutan();
+
+// function urutanWhile() {
+//     let arr = ["warung", "ke", "jalan", "saya"];
+//     let newArr = [];
+//     let i = arr.length - 1;
+//     while (i >= 0) {
+//         newArr.push(arr[i]);
+//         i--;
+//     }
+//     console.info(newArr);
+// }
+// urutanWhile();
