bc6342ba0ca804c2f01b9bce3039c0fedc0be837
Saydina <saydinambiya@gmail.com>
2022-12-23 16:46:13 +0700

    [ADD] variable javascript

---
+++ variable.js
@@ -0,0 +1,16 @@
+let var1 = 2;
+const var2 = 3;
+
+let firstName = "Josh";
+let lastName = "Esyeden";
+let dob = "16/4/2000";
+let hobby = "Movies";
+let education = "D4";
+
+let identity = {
+    nameObj: "Josh",
+    hobbyObj: "Movies"
+}
+
+console.log(identity);
+// console.log(`First Name: ${firstName}, Last Name: ${lastName}, Date of Birth: ${dob}, Hobby: ${hobby}, Education: ${education}`);
